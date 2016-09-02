861de84c95e5d6d24ed6ab80434d9e2a8b9b313c
jsanchez421 <jsanchez421@upc.edu.co>
2016-09-01 21:09:24 -0500

    otraprueba

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'gmgcjwawatv599gq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'f1ad40igeoq4hi3p');//Usuario de tu base de datos
+define('DB_PASS', 'kum3vfxj5f81xnpx');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'g73pvroop2if4sj5');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'NPI');
+define('DIRECCION_EMPRESA', 'BOGOTA COLOMBIA');
+define('TELEFONO_EMPRESA', '+(503) 5555555');
+define('EMAIL_EMPRESA', 'NPI@SBDIBO.COM');
+define('TAX', '13');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
